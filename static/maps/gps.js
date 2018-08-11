d74430dccb6c7afd4eaf3f1b4f429e50c7b9b537
ilius33 <ilius33@mail.ru>
2018-08-11 21:48:25 +0400

    autochanging area maps
    The idea is to automagically change map in already opened map window when exiting current area.

--- static/maps/gps.js
+++ static/maps/gps.js
@@ -1,24 +1,34 @@
 
 if('BroadcastChannel' in window) {
     var bc = new BroadcastChannel('location');
-
+    var prevArea;
     bc.onmessage = function (ev) {
         if(ev.data.what !== 'location')
             return;
 
+        var currentArea = ev.data.location.area;
+
+        if (typeof prevArea === 'undefined') {
+            prevArea = currentArea;
+        }
+
+        if (currentArea != prevArea) {
+            window.location.href = '/maps/' + currentArea.replace(/\.are$/, '') + '.html?1';
+        }
+
         $('.room').removeClass('active');
         $('.room-' + ev.data.location.vnum).addClass('active');
 
-	var $active = $('.room.active'),
-	    $follow = $('#follow');
+        var $active = $('.room.active'),
+        $follow = $('#follow');
 
-	if($active.length && $follow.length && $follow[0].checked) {
-	    var off = $active.offset(),
-		$vp = $('html, body');
+        if($active.length && $follow.length && $follow[0].checked) {
+            var off = $active.offset(),
+            $vp = $('html, body');
 
-	    $vp.scrollTop(off.top-$(window).height()/2);
-	    $vp.scrollLeft(off.left-$(window).width()/2);
-	}
+            $vp.scrollTop(off.top-$(window).height()/2);
+            $vp.scrollLeft(off.left-$(window).width()/2);
+        }
     };
 
     $(document).ready(function() {
