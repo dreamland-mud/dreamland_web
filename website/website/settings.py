6a18693711af9d6771801300120ff4743b13b0f3
amitkadivar1 <amit.kadivar1@gmail.com>
2018-10-18 15:25:27 +0530

    second change

--- website/website/settings.py
+++ website/website/settings.py
@@ -33,16 +33,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
-    'searcher',
-    'corsheaders',
-    'rest_framework',
-    'django_filters',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'searcher',
+    'corsheaders',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
