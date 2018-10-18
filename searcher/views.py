898f23bf4c7a48687c7e09d469ac13255403c366
amitkadivar1 <amit.kadivar1@gmail.com>
2018-10-18 15:20:05 +0530

    change

--- searcher/views.py
+++ searcher/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttResponse
 # Create your views here.
+def index(request):
+    return HttResponse("Welcome to the site")
