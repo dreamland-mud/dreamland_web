a822bde0d2146ee2f3edff1d7e2178cb61ad0d98
Ruffina Koza <ruffina.koza@gmail.com>
2019-02-27 21:28:52 +1300

    Script to populate Fenia API placeholders.

--- static/make-all.sh
+++ static/make-all.sh
@@ -4,7 +4,7 @@ set -e
 
 cd xslt
 echo ">>> Translating with XSLT:"
-for i in 'index' 'guide' 'links' 'maps' 'stories' 'searcher' 'dev';
+for i in 'index' 'guide' 'links' 'maps' 'stories' 'searcher' 'dev' 'feniaapi';
 do
         echo -n "...$i "
         xsltproc main.xslt $i.xml > ../$i.html
@@ -13,3 +13,10 @@ done
 cd ..
 
 ./make-news.sh
+
+api_dir='/home/dreamland/runtime/var/misc/api'
+for i in `ls $api_dir/*`; do
+    echo "Replacing tag $i...";
+    tag=`basename $i`
+    replace "%$tag%" "`cat $i|iconv -c -f koi8-r -t utf-8`" -- feniaapi.html
+done
