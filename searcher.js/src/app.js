cf5b380a43dc7371c56ce63921c8a58bf5eff57b
Ruffina Koza <ruffina.koza@gmail.com>
2020-02-05 15:20:24 +1300

    Searcher: re-read tables whenever file is changed.

--- searcher.js/src/app.js
+++ searcher.js/src/app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path')
 
-const db_magic = require('/tmp/db_magic.json')
-const db_pets = require('/tmp/db_pets.json')
-const db_armor = require('/tmp/db_armor.json')
-const db_weapon = require('/tmp/db_weapon.json')
+/** Invalidate 'require' cache whenever a file changes on disk. */
+const nocache = module => {
+    fs.watchFile(
+        path.resolve(module), 
+        () => {delete require.cache[require.resolve(module)]}
+    )
+}
 
+/* Init cache invalidation listeners for all tables. */
+nocache('/tmp/db_magic.json')
+nocache('/tmp/db_pets.json')
+nocache('/tmp/db_armor.json')
+nocache('/tmp/db_weapon.json')
+
+/* Request parameter parsing routines. */
 const match = (s1, s2) => !s1 || s2.toLowerCase().includes(s1);
 const number = (value, dflt) => value ? parseInt(value) : dflt;
 const string = s => s ? s.toLowerCase() : s;
 
+/* Profiling/logging routines. */
 const timeLabel = req => req.route.path + ' ' + JSON.stringify(req.query);
 const time = req => console.time(timeLabel(req));
 const timeEnd = req => console.timeEnd(timeLabel(req));
@@ -23,7 +36,7 @@ app.get('/searcher-api/item', (req, res) => {
     let search = string(req.query.search);
 
     res.send( 
-        db_armor.filter(item => 
+        require('/tmp/db_armor.json').filter(item => 
             item.level >= lvl0
             && item.level <= lvl1
             && (!wearloc || item.wearloc === wearloc)
@@ -35,49 +48,61 @@ app.get('/searcher-api/item', (req, res) => {
 });
 
 app.get('/searcher-api/weapon', (req, res) => {
+    time(req);
+
     let lvl0 = number(req.query.level__range_0, -1);
     let lvl1 = number(req.query.level__range_1, 200);
     let wclass = req.query.wclass;
     let search = string(req.query.search);
 
     res.send( 
-        db_weapon.filter(item => 
+        require('/tmp/db_weapon.json').filter(item => 
             item.level >= lvl0
             && item.level <= lvl1
             && (!wclass || item.wclass === wclass)
             && match(search, item.name)
         )
     );
+
+    timeEnd(req);
 });
 
 app.get('/searcher-api/pet', (req, res) => {
+    time(req);
+
     let lvl0 = number(req.query.level__range_0, -1);
     let lvl1 = number(req.query.level__range_1, 200);
     let search = string(req.query.search);
 
     res.send( 
-        db_pets.filter(pet => 
+        require('/tmp/db_pets.json').filter(pet => 
             pet.level >= lvl0
             && pet.level <= lvl1
             && match(search, pet.name)
         )
     );
+
+    timeEnd(req);
 });
 
 app.get('/searcher-api/magicItem', (req, res) => {
+    time(req);
+
     let lvl0 = number(req.query.level__range_0, -1);
     let lvl1 = number(req.query.level__range_1, 200);
     let search = string(req.query.search);
     let itemtype = req.query.itemtype;
 
     res.send( 
-        db_magic.filter(i => 
+        require('/tmp/db_magic.json').filter(i => 
             i.level >= lvl0
             && i.level <= lvl1
             && (!itemtype || i.itemtype === itemtype)
             && (!search || i.spells.includes(search))
         )
     );
+
+    timeEnd(req);
 });
 
 app.listen(8001, '127.0.0.1', () => {
