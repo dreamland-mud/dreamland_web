33e64d92c2521846db378342d8c21ce47edec570
Ruffina Koza <ruffina.koza@gmail.com>
2020-02-29 14:34:48 +1300

    Help: add landing pages for clan and class skills.

--- help.js/help.js
+++ help.js/help.js
@@ -23,13 +23,14 @@ var linksById = new Map(
     dictionary.map(entry => [entry.id, entry])
 )
 var linksByKeyword = new Map()
-for (var i = 0; i < dictionary.length; i++)
-    dictionary[i].kwList.forEach(kw => {
-        if (linksByKeyword.get(kw))
-            console.log('Duplicated keyword', kw, 'for id', dictionary[i].id, 'and', linksByKeyword.get(kw).id)
-        else
-            linksByKeyword.set(kw, dictionary[i])
-    })
+for (var i = 0; i < dictionary.length; i++) 
+    if (dictionary[i].kwList)
+        dictionary[i].kwList.forEach(kw => {
+            if (linksByKeyword.get(kw))
+                console.log('Duplicated keyword', kw, 'for id', dictionary[i].id, 'and', linksByKeyword.get(kw).id)
+            else
+                linksByKeyword.set(kw, dictionary[i])
+        })
 
 console.log('Created', linksById.size, 'mappings from id to topic, and', linksByKeyword.size, ' - from keyword to topic');
 
@@ -83,7 +84,7 @@ function topicTitle(topic, labels) {
            return topic.titles[labels[i]];
 }
 
-function saveCategory(labels, title) {
+function saveCategory(labelsInclude, labelsExclude, title) {
     let topics = [];
     for (var i = 0; i < dictionary.length; i++) {        
         let topic = dictionary[i]
@@ -93,23 +94,26 @@ function saveCategory(labels, title) {
             continue
         }
 
-        if (!labels.some(l => topic.labels.includes(l))) 
+        if (!labelsInclude.some(l => topic.labels.includes(l))) 
+            continue
+
+        if (labelsExclude.some(l => topic.labels.includes(l))) 
             continue
 
         let t = {}
         t.kw = topic.kw
-        t.title = topicTitle(topic, labels)
+        t.title = topicTitle(topic, labelsInclude)
         t.text = transformText(topic.text)
         t.id = topic.id
         topics.push(t)
     }
 
     if (topics.length == 0) {
-        console.log("No topics found for labels", labels)
+        console.log("No topics found for labels", labelsInclude)
         return
     }
 
-    labels.forEach(label =>    
+    labelsInclude.forEach(label =>    
        ejs.renderFile(
            'templates/help-category.ejs', 
            {
@@ -123,39 +127,69 @@ function saveCategory(labels, title) {
     )
 }
 
-saveCategory(['race'], 'Расы');
-saveCategory(['class'], 'Классы');
-saveCategory(['religion'], 'Религии');
-saveCategory(['clan'], 'Кланы');
-saveCategory(['skill'], 'Умения');
-saveCategory(['spell'], 'Заклинания');
-saveCategory(['skillgroup'], 'Группы умений и заклинаний');
-saveCategory(['area'], 'Зоны');
-saveCategory(['social'], 'Социалы');
-saveCategory(['cmd'], 'Все команды');
-saveCategory(['quest'], 'Квесты');
-saveCategory(['char'], 'Персонаж');
-saveCategory(['info'], 'Информация');
-saveCategory(['shop', 'bank', 'service' ], 'Торговля и услуги');
-saveCategory(['learn'], 'Обучение');
-saveCategory(['item', 'food'], 'Предметы');
-saveCategory(['move', 'locks', 'position'], 'Перемещение');
-saveCategory(['fight'], 'Битва');
-saveCategory(['magic'], 'Магия');
-saveCategory(['note'], 'Переписка');
-saveCategory(['comm', 'family'], 'Общение и семья'); 
-saveCategory(['genericskill'], 'Профессиональные навыки');
-saveCategory(['raceaptitude'], 'Расовые навыки');
-saveCategory(['clanskill'], 'Клановые навыки');
-saveCategory(['craftskill'], 'Крафт');
-saveCategory(['craft'], 'Крафт');
-saveCategory(['cardskill'], 'Навыки картежника');
-saveCategory(['language'], 'Языки');
+saveCategory(['race'], [], 'Расы');
+saveCategory(['class'], [], 'Классы');
+saveCategory(['religion'], [], 'Религии');
+saveCategory(['clan'], [], 'Кланы');
+saveCategory(['skill'], [], 'Умения');
+saveCategory(['spell'], [], 'Заклинания');
+saveCategory(['skillgroup'], [], 'Группы умений и заклинаний');
+saveCategory(['area'], [], 'Зоны');
+saveCategory(['social'], [], 'Социалы');
+saveCategory(['cmd'], [], 'Все команды');
+saveCategory(['quest'], [], 'Квесты');
+saveCategory(['char'], [], 'Персонаж');
+saveCategory(['info'], [], 'Информация');
+saveCategory(['shop', 'bank', 'service' ], [], 'Торговля и услуги');
+saveCategory(['learn'], [], 'Обучение');
+saveCategory(['item', 'food'], [], 'Предметы');
+saveCategory(['move', 'locks', 'position'], [], 'Перемещение');
+saveCategory(['fight'], [], 'Битва');
+saveCategory(['magic'], [], 'Магия');
+saveCategory(['note'], [], 'Переписка');
+saveCategory(['comm', 'family'], [], 'Общение и семья'); 
+saveCategory(['genericskill'], ['language'], 'Профессиональные навыки');
+saveCategory(['raceaptitude'], [], 'Расовые навыки');
+saveCategory(['clanskill'], [], 'Клановые навыки');
+saveCategory(['craftskill'], [], 'Крафт');
+saveCategory(['craft'], [], 'Крафт');
+saveCategory(['cardskill'], [], 'Навыки картежника');
+saveCategory(['language'], [], 'Языки');
 // +config, +client, +misc
 // +group
 
+saveCategory(['warrior-skills'], [], 'Все навыки воина');
+saveCategory(['thief-skills'], [], 'Все навыки вора');
+saveCategory(['cleric-skills'], [], 'Все навыки клерика');
+saveCategory(['witch-skills'], [], 'Все навыки ведьмы');
+saveCategory(['warlock-skills'], [], 'Все навыки колдуна (варлока)');
+saveCategory(['ranger-skills'], [], 'Все навыки рейнджера');
+saveCategory(['samurai-skills'], [], 'Все навыки самурая');
+saveCategory(['ninja-skills'], [], 'Все навыки ниндзи');
+saveCategory(['paladin-skills'], [], 'Все навыки паладина');
+saveCategory(['anti-paladin-skills'], [], 'Все навыки анти-паладина');
+saveCategory(['necromancer-skills'], [], 'Все навыки некроманта');
+saveCategory(['vampire-skills'], [], 'Все навыки вампира');
+
+saveCategory(['lion-skills'], [], 'Навыки клана Львов (Lion)');
+saveCategory(['battlerager-skills'], [], 'Навыки клана Ярости (Battlerager)');
+saveCategory(['knight-skills'], [], 'Навыки клана Рыцарей (Knight)');
+saveCategory(['chaos-skills'], [], 'Навыки клана Хаоса (Chaos)');
+saveCategory(['shalafi-skills'], [], 'Навыки клана Шалафи (Shalafi)');
+saveCategory(['ruler-skills'], [], 'Навыки клана Правителей (Ruler)');
+saveCategory(['invader-skills'], [], 'Навыки клана Захватчиков (Invader)');
+saveCategory(['hunter-skills'], [], 'Навыки клана Охотников (Hunter)');
+
 ejs.renderFile('templates/help-index.ejs', function(err, str) {
     fs.writeFileSync(destDir + '/index.html', str)
+})
+
+ejs.renderFile('templates/landing-classskill.ejs', function(err, str) {
+    fs.writeFileSync(destDir + '/classskill.html', str)
+})
+
+ejs.renderFile('templates/landing-clanskill.ejs', function(err, str) {
+    fs.writeFileSync(destDir + '/clanskills.html', str)
 })
 
 
